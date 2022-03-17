4d649aa903540ee0bd6f265cf94debb92bacb3bd
GaryTin <tinkwaiki@gmail.com>
2022-03-18 00:36:13 +0800

    website_setup

--- hackUST_2022_B_Sharp/hackUST_2022_B_Sharp/settings.py
+++ hackUST_2022_B_Sharp/hackUST_2022_B_Sharp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g3u3wuu$=+i#!=@*i5d0g356z@4$jhqo)qeu9bq%xyzku)%_%(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'SharpBargain',
 ]
 
 MIDDLEWARE = [
