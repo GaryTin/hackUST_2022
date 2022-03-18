420e78a7f4f432262c1481cd7c916d221ad373f9
GaryTin <tinkwaiki@gmail.com>
2022-03-18 17:38:48 +0800

    Database Draft

--- hackUST_2022_B_Sharp/SharpBargain/models.py
+++ hackUST_2022_B_Sharp/SharpBargain/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
+
 # Create your models here.
+class DB_user(models.Model):
+    user_id = models.IntegerField(unique=True)
+    email = models.EmailField()
+    password = models.CharField(max=32)
+    ROLE_CHOICE = [
+        ('C', 'Customer'),
+        ('R', 'Retailer'),
+        ('M', 'Manufacturer'),
+    ]
+
+    role = models.CharField(
+        max_length=1,
+        choices=ROLE_CHOICE,
+        default='C'
+    )
+
+    name = models.CharField(max_length=256)
+
+    objects = models.Manager()
+
+class DB_Product(models.Model):
+    prod_type = models.CharField()
+    prod_img = models.ImageField(upload_to="images")
+
+    objects = models.Manager()
+
+class Manufacturer(models.Model):
+    manu_id = models.IntegerField(unique=True)
+    manu_name = models.CharField()
+    manu_address = models.CharField()
+
+    objects = models.Manager()
+
+
